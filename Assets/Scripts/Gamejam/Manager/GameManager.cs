332dba86ea1173d3a0c904218a2f1df3701a47cc
palulski <paul.scheiner.regensburg@gmail.com>
2025-03-14 21:33:18 +0100

    Escape routine

--- Assets/Scripts/Gamejam/Manager/GameManager.cs
+++ Assets/Scripts/Gamejam/Manager/GameManager.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections;
 using System.ComponentModel.Design.Serialization;
+using Mono.CSharp;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float escapeTimer = 15f;
     public bool gameIsActive = true;
     public static GameManager instance;
 
@@ -19,9 +22,22 @@ public class GameManager : MonoBehaviour
         EventManager.instance.AlarmEvent += alarm;
     }
     public void timesUp(object sender, EventArgs e) {
-        Debug.Log("Times Up");
+        gameover();
     }
     public void alarm(object sender, EventArgs e){
-        Debug.Log("alarm");
+        StartCoroutine(Escape());
+    }
+    IEnumerator Escape(){
+        while(escapeTimer > 0){
+            yield return new WaitForSeconds(1);
+            escapeTimer -= 1;
+            if(escapeTimer == 0){
+                gameover();
+            }
+        }
+        
+    }
+    public void gameover(){
+        Debug.Log("game over");
     }
 }
