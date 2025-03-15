68376700656db0c7bbee32abdcd575a808dc2ee2
fridolinderkater <148785097+fridolinderkater@users.noreply.github.com>
2025-03-15 12:03:55 +0100

    Noise Smooth

--- Assets/Scripts/Gamejam/Manager/NoiseManager.cs
+++ Assets/Scripts/Gamejam/Manager/NoiseManager.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 public class NoiseManager : MonoBehaviour
 {
     public float noise;
+    private ManagerUI manager;
 
-    void Update() {
-        this.GetComponent<ManagerUI>().UpdateUI(noise, 100f);
-    }
     private void Start() {
+        manager = this.GetComponent<ManagerUI>();
         EventManager.instance.MakeNoiseEvent += addNoise;
-        StartCoroutine(noiseCheck());
+        //StartCoroutine(noiseCheck());
     }
-    IEnumerator noiseCheck(){
-        while(GameManager.instance.gameIsActive){
-            yield return new WaitForSeconds(1);
-            if(noise > 0){
-                noise -= 1;
-            }
-            if(noise >= 100){
-                noise = 100;
-                EventManager.instance.Alarm();
-            }
+    
+    void Update() {
+        manager.UpdateUI(noise, 100f);
+        UpdateNoise();
+    }
+
+    public void UpdateNoise() {
+        if(noise > 0) {
+            noise -= Time.deltaTime * 2f;
         }
-    } 
-    private void addNoise(object sender, NoiseEvent e){
+
+        if(noise >= 100f){
+            noise = 100f;
+            EventManager.instance.Alarm();
+        }
+    }
+    
+    // IEnumerator noiseCheck(){
+    //     while(GameManager.instance.gameIsActive){
+    //         yield return new WaitForSeconds(1);
+    //         if(noise > 0){
+    //             noise -= 1;
+    //         }
+    //         if(noise >= 100){
+    //             noise = 100;
+    //             EventManager.instance.Alarm();
+    //         }
+    //     }
+    // } 
+    void addNoise(object sender, NoiseEvent e){
         noise += e.noise;
         Debug.Log("Laut");
     } 
