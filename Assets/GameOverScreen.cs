f1c44576d5d126be3c84c9928884a9d4a1942f3d
Muawag <101280259+Muawag@users.noreply.github.com>
2025-03-15 21:17:49 +0100

    agtawgaw

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -12,7 +12,6 @@ public class GameOverScreen : MonoBehaviour
     {
         cGroup = GetComponent<CanvasGroup>();
         cGroup.alpha = 0f;
-        Setup(false);
     }
 
     public void Setup(bool won)
